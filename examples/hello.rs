5c332819ca9a1ea5981a14515db94c6dedcb3479
saltyfishie-opensuse <teugene888@gmail.com>
2024-05-25 02:49:29 +0800

    added basic actor termination

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,7 +1,10 @@
+use std::{thread::sleep, time::Duration};
+
 use pelakon::*;
 
-struct MyMessage {
-    data: u32,
+enum MyMessage {
+    Data(u32),
+    Exit,
 }
 impl Message for MyMessage {}
 
@@ -11,21 +14,43 @@ struct MyMessage2 {
 impl Message for MyMessage2 {}
 
 struct MyActor;
-impl Actor for MyActor {}
+impl Actor for MyActor {
+    fn on_entry(&mut self) {
+        println!("hello!");
+    }
+    fn on_exit(&mut self) {
+        println!("exited!");
+    }
+}
 impl Received<MyMessage> for MyActor {
-    fn process(&mut self, msg: MyMessage) {
-        println!("{}", msg.data);
+    fn process(&mut self, msg: MyMessage, ctx: &mut Context<MyActor>) {
+        match msg {
+            MyMessage::Data(data) => {
+                if data == 0 {
+                    sleep(Duration::from_secs(5));
+                }
+                println!("MyMassage.data: {}", data);
+            }
+            MyMessage::Exit => ctx.terminate(),
+        };
     }
 }
 impl Received<MyMessage2> for MyActor {
-    fn process(&mut self, msg: MyMessage2) {
-        println!("{}", msg.data);
+    fn process(&mut self, msg: MyMessage2, _: &mut Context<MyActor>) {
+        println!("MyMassage2.data: {}", msg.data);
     }
 }
 
 fn main() {
     let handle = MyActor.start();
-    handle.send(MyMessage { data: 42 });
+    let sender = handle.clone_sender();
+    ctrlc::set_handler(move || {
+        sender.send(MyMessage::Exit);
+    })
+    .unwrap();
+
+    handle.send(MyMessage::Data(0));
     handle.send(MyMessage2 { data: 42 });
-    handle.thread_handle.join().unwrap();
+
+    handle.join().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
     sync::mpsc,
-    thread::{self, JoinHandle},
+    thread::{self, JoinHandle, Result},
 };
 
 pub trait Message {}
@@ -9,7 +9,7 @@ pub trait Received<T: Message>
 where
     Self: Actor,
 {
-    fn process(&mut self, msg: T);
+    fn process(&mut self, msg: T, ctx: &mut Context<Self>);
 }
 
 pub trait Actor: Sized + Send + 'static {
@@ -17,18 +17,18 @@ pub trait Actor: Sized + Send + 'static {
         Context::new().run(self)
     }
 
-    fn hello(&self) {}
+    fn on_entry(&mut self) {}
+    fn on_exit(&mut self) {}
 }
 
 trait EnvelopeCallProxy<A: Actor> {
-    fn process(&mut self, act: &mut A);
+    fn process(&mut self, act: &mut A, ctx: &mut Context<A>);
 }
 
-pub struct Handle<A: Actor> {
-    pub tx: mpsc::Sender<Envelope<A>>,
-    pub thread_handle: JoinHandle<()>,
+pub struct Sender<A: Actor> {
+    tx: mpsc::Sender<Envelope<A>>,
 }
-impl<A: Actor> Handle<A> {
+impl<A: Actor> Sender<A> {
     pub fn send<M>(&self, msg: M)
     where
         M: Message + Send + 'static,
@@ -37,27 +37,70 @@ impl<A: Actor> Handle<A> {
         self.tx.send(Envelope::new(msg)).unwrap();
     }
 }
+impl<A: Actor> Clone for Sender<A> {
+    fn clone(&self) -> Self {
+        Self {
+            tx: self.tx.clone(),
+        }
+    }
+}
+
+pub struct Handle<A: Actor> {
+    sender: Sender<A>,
+    thread_handle: JoinHandle<()>,
+}
+impl<A: Actor> Handle<A> {
+    pub fn send<M>(&self, msg: M)
+    where
+        M: Message + Send + 'static,
+        A: Received<M>,
+    {
+        self.sender.send(msg);
+    }
+
+    pub fn clone_sender(&self) -> Sender<A> {
+        self.sender.clone()
+    }
+
+    pub fn join(self) -> Result<()> {
+        self.thread_handle.join()
+    }
+}
 
 pub struct Context<A: Actor> {
     mailbox: (mpsc::Sender<Envelope<A>>, mpsc::Receiver<Envelope<A>>),
+    exit: bool,
 }
 impl<A: Actor> Context<A> {
     fn new() -> Self {
         Self {
             mailbox: mpsc::channel(),
+            exit: false,
         }
     }
 
-    fn run(self, mut actor: A) -> Handle<A> {
+    fn run(mut self, mut actor: A) -> Handle<A> {
         let tx = self.mailbox.0.clone();
 
-        let thread_handle = thread::spawn(move || loop {
+        let thread_handle = thread::spawn(move || {
+            actor.on_entry();
             while let Ok(mut msg) = self.mailbox.1.recv() {
-                msg.data.process(&mut actor);
+                msg.data.process(&mut actor, &mut self);
+                if self.exit {
+                    break;
+                }
             }
+            actor.on_exit();
         });
 
-        Handle { tx, thread_handle }
+        Handle {
+            sender: Sender { tx },
+            thread_handle,
+        }
+    }
+
+    pub fn terminate(&mut self) {
+        self.exit = true;
     }
 }
 
@@ -87,9 +130,9 @@ where
     M: Message + Send + 'static,
     A: Actor + Received<M>,
 {
-    fn process(&mut self, act: &mut A) {
+    fn process(&mut self, act: &mut A, ctx: &mut Context<A>) {
         if let Some(msg) = self.msg.take() {
-            <A as Received<M>>::process(act, msg);
+            <A as Received<M>>::process(act, msg, ctx);
         }
     }
 }
